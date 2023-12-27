caaf3b1440f45439f6d7b1b65d0662d8231ac2e7
Abel Andrés <abelandres.gtc@gmail.com>
2023-12-27 08:46:58 +0100

    remove unused function

--- server/jwt/jwt.go
+++ server/jwt/jwt.go
@@ -7,32 +7,12 @@ import (
 
 	"github.com/fabiocicerchia/go-proxy-cache/config"
 	"github.com/lestrrat-go/backoff/v2"
-	"github.com/lestrrat-go/jwx/jwa"
 	"github.com/lestrrat-go/jwx/jwk"
 	"github.com/lestrrat-go/jwx/jwt"
 )
 
 var co *config.Jwt
 var jwtKeyFetcher *jwk.AutoRefresh
-
-// TODO: Remove CreateJWT if unused
-func CreateJWT(key []byte) (string, error) {
-	claims := jwt.New()
-	// claims.Set(jwt.IssuerKey, "issuer")
-	// claims.Set(jwt.AudienceKey, "audience_key")
-	// claims.Set(jwt.ExpirationKey, time.Now().Add(1*time.Hour))
-	// claims.Set(jwt.NotBeforeKey, time.Now().Add(-1*time.Minute))
-	// claims.Set(jwt.IssuedAtKey, time.Now())
-	// claims.Set(jwt.JwtIDKey, "token_id")
-	claims.Set("scope", []string{"scope1", "scope2", "scope3"})
-
-	token, err := jwt.Sign(claims, jwa.HS256, key)
-	if err != nil {
-		return "", err
-	}
-
-	return string(token), nil
-}
 
 func InitJWT(conifg *config.Jwt) {
 
